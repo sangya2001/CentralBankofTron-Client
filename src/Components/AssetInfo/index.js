6e265d52927eeb61f27e04497c334522f8e82213
sangya2001 <sangyasherpa2058@gmail.com>
2020-12-23 22:14:28 +0545

    Changing to lex

--- src/Components/AssetInfo/index.js
+++ src/Components/AssetInfo/index.js
@@ -81,7 +81,7 @@ export default function AssetInfo({ contract, setIsLoading }) {
                                 }}
                                     disabled={isWithdrawableLocalStorage}
                                 >
-                                    Withdraw Dividend 
+                                    Withdraw 
                                     {
                                         (localStorage.getItem('canWithdrawAt') !== null) && 
                                             <span> - 
